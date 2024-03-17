7c5a167d3e97faa2fad9dde53e353b2fbac36b37
Ammar Owis <aowis@emich.edu>
2024-03-17 22:36:50 +0000

    Created menu for the movie program with a quit option and a feature for re=entry if input is invalid

--- query.py
+++ query.py
@@ -1,3 +1,22 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+def show_menu():
+    print("Menu:")
+    print("q. Quit")
+
+def main():
+    movies = Movies('./movies.txt') 
+
+    while True:
+        show_menu()
+        choice = input("Enter your choice: ").strip().lower()
+
+        if choice == 'q':
+            print("Exiting program...")
+            break
+        else:
+            print("Invalid option.")
+
+if __name__ == "__main__":
+    main()
